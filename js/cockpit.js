a135ea14887f0abdb60c617be407d6de4551b72d
WH ZHAO <wenhuipeter98@gmail.com>
2019-12-02 22:56:38 -0500

    .

---
+++ js/cockpit.js
@@ -0,0 +1,57 @@
+
+document.onreadystatechange = () => {
+    if (document.readyState === 'complete') {
+        var cockpit = document.querySelector("#cockpit");
+        var header = document.querySelector("#header");
+        var iframe = document.querySelector("iframe");
+        var content_in_cockpit = document.querySelector("#content_in_cockpit");
+            cockpit.classList.add("zoom_out");
+            header.classList.add("zoom_out","translate_down");
+            cockpit.addEventListener("animationend",function () {
+                content_in_cockpit.style.display ="block";
+                content_in_cockpit.classList.add("fade_in");
+            });
+
+
+        var navi_earth = document.querySelector("#navi_earth");
+        var navi_mars = document.querySelector("#navi_mars");
+        var navi_venus = document.querySelector("#navi_venus");
+        var go = document.querySelector("#go");
+        var target = '';
+        var can_go = false;
+
+        navi_earth.onclick=function () {
+            show("earth");
+            target = 'earth.html';
+            go.style.color ='white';
+            can_go = true;
+        };
+
+        navi_mars.onclick=function () {
+            show("mars");
+            target = 'mars.html';
+            go.style.color ='white';
+            can_go = true;
+        };
+
+        navi_venus.onclick=function () {
+            show("venus");
+            target = 'venus.html';
+            go.style.color ='white';
+            can_go = true;
+        };
+
+        go.onclick = function () {
+            if(can_go) window.location.href = target;
+                content_in_cockpit.style.opacity ="100%";
+                iframe.style.display="block";
+        }
+    };
+
+
+};
+
+//
+// var can_go = function (a,b,c) {
+//     if()
+// }
--- js/index.js
+++ js/index.js
@@ -2,55 +2,10 @@
 document.onreadystatechange = () => {
     if (document.readyState === 'complete') {
         var welcome_button = document.querySelector( '#welcome_sec .button' );
-        var welcome_sec = document.querySelector("#welcome_sec");
-        var cockpit = document.querySelector("#cockpit");
-        var header = document.querySelector("#header");
-        var iframe = document.querySelector("iframe");
-        var content_in_cockpit = document.querySelector("#content_in_cockpit");
-        console.log(welcome_button);
+
+
         welcome_button.onclick = function () {
-            welcome_sec.classList.add("fade_out");
-            cockpit.classList.add("zoom_out");
-            header.classList.add("zoom_out","translate_down");
-            welcome_sec.addEventListener("animationend",function () {
-                welcome_sec.style.display ="none";
-                content_in_cockpit.style.display ="block";
-                content_in_cockpit.classList.add("fade_in");
-            });
-        };
-
-        var navi_earth = document.querySelector("#navi_earth");
-        var navi_mars = document.querySelector("#navi_mars");
-        var navi_venus = document.querySelector("#navi_venus");
-        var go = document.querySelector("#go");
-        var target = '';
-        var can_go = false;
-
-        navi_earth.onclick=function () {
-            show("earth");
-            target = 'earth.html';
-            go.style.color ='white';
-            can_go = true;
-        };
-
-        navi_mars.onclick=function () {
-            show("mars");
-            target = 'mars.html';
-            go.style.color ='white';
-            can_go = true;
-        };
-
-        navi_venus.onclick=function () {
-            show("venus");
-            target = 'venus.html';
-            go.style.color ='white';
-            can_go = true;
-        };
-
-        go.onclick = function () {
-            if(can_go) window.location.href = target;
-                content_in_cockpit.style.opacity ="100%";
-                iframe.style.display="block";
+            window.location.href = "cockpit.html";
         }
     };
 
