389d0270a2d0df49fdf30baeddbed77af7a8883b
WH ZHAO <wenhuipeter98@gmail.com>
2019-12-02 21:24:43 -0500

    .

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ document.onreadystatechange = () => {
         var welcome_sec = document.querySelector("#welcome_sec");
         var cockpit = document.querySelector("#cockpit");
         var header = document.querySelector("#header");
+        var iframe = document.querySelector("iframe");
         var content_in_cockpit = document.querySelector("#content_in_cockpit");
         console.log(welcome_button);
         welcome_button.onclick = function () {
@@ -16,10 +17,29 @@ document.onreadystatechange = () => {
                 content_in_cockpit.style.display ="block";
                 content_in_cockpit.classList.add("fade_in");
                 content_in_cockpit.style.opacity ="100%";
+                iframe.style.display="block";
         })
+    };
+
+        var navi_earth = document.querySelector("#navi_earth");
+        var navi_mars = document.querySelector("#navi_mars");
+        var navi_venus = document.querySelector("#navi_venus");
+
+        navi_earth.onclick=function () {
+            show('earth');
+        };
+
+        navi_mars.onclick=function () {
+            show('mars');
+        };
+
+        navi_venus.onclick=function () {
+            show('venus');
+        };
+
+
     }
-}
-}
+};
 
 //
 // var can_go = function (a,b,c) {
